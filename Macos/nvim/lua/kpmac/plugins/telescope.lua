1cf9b4d721ee05562f064cef05506332095d5daa
kushalpawaskar <kushalpawaskar02@gmail.com>
2025-02-20 23:27:13 +0530

    update telescope config

--- Macos/nvim/lua/kpmac/plugins/telescope.lua
+++ Macos/nvim/lua/kpmac/plugins/telescope.lua
@@ -25,6 +25,9 @@ return {
                 find_files = {
                     find_command = { "rg", "--files", "--hidden", "-g", "!.git" },      -- to show hidden files as well excluding the ones in .git
                 },
+                buffers = {
+                    sort_mru = true,        -- sort buffers by most recently used
+                },
             },
         })
 
@@ -37,6 +40,7 @@ return {
         keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
         keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
         keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find open buffers" })
 
     end
 }
